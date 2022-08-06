8f7e8eb0c639ea72e86f0073850fbba013e124ea
black <gikigiio@gmail.com>
2022-08-06 19:08:50 -0300

    Req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,15 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
+
+path = "src/jobs.csv"
+
+word_javascript = "javascript"
+word_typescript = "typescript"
 
 
 def test_counter():
-    pass
+    try:
+        count = count_ocurrences(path, word_javascript)
+        print(count)
+        assert count == 122
+    except ValueError:
+        assert False
